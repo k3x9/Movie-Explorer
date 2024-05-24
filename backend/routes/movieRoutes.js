e79fa01574e3bb11f5dac36638d0cd7058254277
k3x9 <adarsh.shrivastava2003@gmail.com>
2024-05-25 01:50:57 +0530

    backedn done

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getMoviesByGenre, getMoviesByTitle } = require('../controllers/movieControllers');
+const { getMoviesByGenre, getMoviesByTitle, getTrendingMovies, getMovieByID} = require('../controllers/movieControllers');
 
-router.get('/genre/:genre', getMoviesByGenre);
-router.get('/title/:title', getMoviesByTitle);
+router.get('/', getTrendingMovies);
+router.get('/genres', getMoviesByGenre);
+router.get('/title', getMoviesByTitle);
+router.get('/:id', getMovieByID);
 
 module.exports = router;
