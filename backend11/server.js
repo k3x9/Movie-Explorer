daf72ac4cf390f7ea88f30cb8cbd069803ff2481
k3x9 <adarsh.shrivastava2003@gmail.com>
2024-05-25 22:24:31 +0530

    deployment try

---
+++ backend11/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv').config({path: '.env'});
+const MoviesRoutes = require('./routes/movieRoutes');
+const cors = require('cors');
+
+app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+    credentials: true,
+    origin: ['*']
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/api/movies", MoviesRoutes);
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
